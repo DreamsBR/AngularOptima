98a891f4237029580c91ef1ab1f78f390eef9143
Raúl Carrillo <raul@MacBook-Air-de-Raul.local>
2020-11-10 15:23:11 -0500

    url de json para pruebas

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
 export const URL_BACKEND = 'http://104.248.230.75:8080/services/v1/';
-export const URL_BACKEND_DEMO = 'http://localhost:4200/assets/json/';
+export const URL_BACKEND_DEMO = '/assets/json/';
