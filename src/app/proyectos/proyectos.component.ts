112764245e14a51a443492a3ddfb1c4081794e04
recemace <receapd@gmail.com>
2020-11-14 17:26:46 -0500

    corrigiendo etiquetas v6

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
+  status = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  menuToggle() {
+    this.status = !this.status;
   }
 
 }
