dacbed8f72d3c3fce1fbc54fdd302a6c5bb2aa00
Raúl Carrillo <raul@MacBook-Air-de-Raul.local>
2020-11-17 20:03:35 -0500

    agregar cliente

--- src/app/clientes-nuevo-editar/clientes-nuevo-editar.component.ts
+++ src/app/clientes-nuevo-editar/clientes-nuevo-editar.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Cliente } from './../clientes/cliente'
+import { ClienteService } from './../clientes/clientes.service'
+import { Router, ActivatedRoute } from '@angular/router'
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes-nuevo-editar',
   templateUrl: './clientes-nuevo-editar.component.html'
 })
 export class ClientesNuevoEditarComponent implements OnInit {
+  public cliente: Cliente = new Cliente()
+  public errores: string[]
 
-  status = false;
-
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    console.info(this.cliente)
   }
 
+  public agregarCliente(): void {
+    console.info(this.cliente)
+    this.cliente.idCliente = 0
+    this.cliente.idPais = 1
+    this.clienteService.agregarCliente(this.cliente).subscribe(
+      (response) => {
+        this.router.navigate(['/clientes'])
+        swal('Nuevo cliente', `Cliente ${response.cliente.nombre} creado con exito`, 'success')
+      },
+      (err) => {
+        this.errores = err.error.errors as string[]
+      }
+    )
+  }
+
+  status = false
   menuToggle() {
-    this.status = !this.status;
+    this.status = !this.status
   }
-
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,70 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente } from './cliente';
-import { ClienteService } from './clientes.service';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '../usuarios/auth.service';
-import { URL_BACKEND } from '../config/config';
+import { Component, OnInit } from '@angular/core'
+import { Cliente } from './cliente'
+import { ClienteService } from './clientes.service'
+import { ActivatedRoute } from '@angular/router'
+import { AuthService } from '../usuarios/auth.service'
+import { URL_BACKEND } from '../config/config'
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
-
 export class ClientesComponent implements OnInit {
-
-  clientesLista: Cliente[];
-  clienteSeleccionado: Cliente;
-  paginador: any;
-  base: string;
-  urlBackend: String = URL_BACKEND;
+  clientesLista: Cliente[]
+  clienteSeleccionado: Cliente
+  paginador: any
+  base: string
+  urlBackend: String = URL_BACKEND
 
   constructor(
     private clienteService: ClienteService,
     private activatedRoute: ActivatedRoute,
     public authService: AuthService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.obtenerCliente();
+    this.obtenerCliente()
   }
 
-  public obtenerCliente(){
-    this.activatedRoute.paramMap.subscribe(params => {
-      let page: number = +params.get('page');
+  public obtenerCliente() {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let page: number = +params.get('page')
       if (!page) {
-        page = 0;
+        page = 0
       }
-      this.clienteService.getClientes(page).subscribe(
-        clientesJsonResponse => {
-          this.clientesLista = clientesJsonResponse.content;
-          this.paginador = clientesJsonResponse;
-          this.base = "cliente";
-        }
-      );
-    });
+      this.clienteService.getClientes(page).subscribe((clientesJsonResponse) => {
+        this.clientesLista = clientesJsonResponse.content
+        this.paginador = clientesJsonResponse
+        this.base = 'cliente'
+      })
+    })
   }
 
   public eliminar(cliente: Cliente): void {
     this.clienteService.eliminarCliente(cliente.idCliente).subscribe(
-      response => {
-        console.info(response);
-        document.getElementById("cerrarModalEliminar").click();
-        this.obtenerCliente();
+      (response) => {
+        console.info(response)
+        document.getElementById('cerrarModalEliminar').click()
+        this.obtenerCliente()
       },
-      err => {
-        console.error(err);
-        document.getElementById("cerrarModalEliminar").click();
-        this.obtenerCliente();
+      (err) => {
+        console.error(err)
+        document.getElementById('cerrarModalEliminar').click()
+        this.obtenerCliente()
       }
     )
   }
 
   public obtenerClienteSeleccionado(cliente: Cliente) {
-    this.clienteSeleccionado = cliente;
+    this.clienteSeleccionado = cliente
   }
 
-  status = false;
+  status = false
   menuToggle() {
-    this.status = !this.status;
+    this.status = !this.status
   }
 }
