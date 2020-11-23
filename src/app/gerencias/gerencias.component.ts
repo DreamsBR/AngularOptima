d4b2ac1d24e5025e4495b9b572c484570f362263
Eddy Erazo <eerazozamudio@outlook.com>
2020-11-23 17:15:58 -0500

    Gerencia

--- src/app/gerencias/gerencias.component.ts
+++ src/app/gerencias/gerencias.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
+import { Colaborador } from '../colaboradores/colaborador';
+import { ColaboradorService } from '../colaboradores/colaborador.service';
 
 @Component({
   selector: 'app-gerencias',
   templateUrl: './gerencias.component.html'
 })
 export class GerenciasComponent implements OnInit {
+  public colaboradorSelecionado:Colaborador = new Colaborador()
 
+  public nrdoc: string
 
-
-  constructor() { }
+  constructor(
+    private colaboradorserv:ColaboradorService
+  ) { }
 
   ngOnInit() {
+    this.colaboradorSelecionado.idColaborador=0
+    this.colaboradorSelecionado.nombres= ''
+    this.colaboradorSelecionado.apellidos= ''
+    this.colaboradorSelecionado.numeroDocumento= ''
   }
-  
+
   status = false;
   menuToggle() {
     this.status = !this.status;
   }
+
+
+
+
 
 }
