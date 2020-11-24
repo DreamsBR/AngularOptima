e00bd4f72575f3415eb71f5ae7560a52a4875df1
raul <raulcarrillo@outlook.com>
2020-11-24 14:19:44 -0500

    consulta de venta

--- src/app/ventas-proyecto-nuevo-editar/ventas-proyecto-nuevo-editar.component.ts
+++ src/app/ventas-proyecto-nuevo-editar/ventas-proyecto-nuevo-editar.component.ts
@@ -266,11 +266,15 @@ export class VentasProyectoNuevoEditarComponent implements OnInit {
     this.financiamiento.idTipoCredito = this.tipocreditoSeleccionado
     this.financiamiento.enable = 1
 
-    let ff = this.fechaFinAhorro.split("-");
-    this.financiamiento.fechaFinAhorro = ( ff[2] + '-' + ff[1] + '-' + ff[0] )
+    //console.info( this.fechaFinAhorro );
 
-    let fi = this.fechaInicioAhorro.split("-");
-    this.financiamiento.fechaInicioAhorro = ( fi[2] + '-' + fi[1] + '-' + fi[0] )
+    //let ff = this.fechaFinAhorro.split("-");
+    //this.financiamiento.fechaFinAhorro = ( ff[2] + '-' + ff[1] + '-' + ff[0] )
+    this.financiamiento.fechaFinAhorro = this.fechaFinAhorro
+
+    //let fi = this.fechaInicioAhorro.split("-");
+    //this.financiamiento.fechaInicioAhorro = ( fi[2] + '-' + fi[1] + '-' + fi[0] )
+    this.financiamiento.fechaFinAhorro = this.fechaInicioAhorro
 
     this.financiamiento.idEstadoFinanciamiento = 1
 
