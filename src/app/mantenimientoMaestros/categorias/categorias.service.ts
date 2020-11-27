8e78a883901bef9673556cab9bb6273cd1716e81
Eddy Erazo <eerazozamudio@outlook.com>
2020-11-27 12:47:55 -0500

    categorias

--- src/app/mantenimientoMaestros/categorias/categorias.service.ts
+++ src/app/mantenimientoMaestros/categorias/categorias.service.ts
@@ -1,2 +1,21 @@
 import { Injectable } from '@angular/core'
+import { Route } from '@angular/router'
+import { URL_BACKEND } from 'src/app/config/config'
+import { inherits } from 'util'
 import { Categorias  } from './categorias'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+
+@Injectable()
+export class CategoriaService {
+  private urlEndPoint : string = URL_BACKEND + 'categoria'
+
+  constructor (
+    private http: HttpClient,
+    private router:Route
+  ){}
+
+
+
+  
+}
