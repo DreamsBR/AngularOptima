22769d29c4f12c360751393ed16ce75e86623386
Eddy Erazo <eerazozamudio@outlook.com>
2020-12-01 16:33:03 -0500

    Filtrado

--- src/app/ventas-proyecto/ventas-proyecto.component.ts
+++ src/app/ventas-proyecto/ventas-proyecto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { Ventasproyecto } from './Ventasproyecto'
 import { Venta } from './../ventas/venta'
 import { VentaService } from './../ventas/ventas.service'
@@ -6,31 +6,55 @@ import { VentasproyectoService } from './ventasproyecto.service'
 import { ActivatedRoute } from '@angular/router'
 import { AuthService } from '../usuarios/auth.service'
 import { Router } from '@angular/router'
+import { estadoventa } from '../consulta-ventas/estadoventa'
+import { DatepickerRoundedComponent } from '../datepicker-rounded/datepicker-rounded.component'
+import { statusVentaservice } from '../consulta-ventas/statusventa.service'
 
 @Component({
   selector: 'app-ventas-proyecto',
   templateUrl: './ventas-proyecto.component.html'
 })
 export class VentasProyectoComponent implements OnInit {
-  
+
   ventasProyectoLista: Ventasproyecto[]
   idProyectoSeleted: number = 0
-
+  tipoestado : estadoventa[]
   paramIdProyecto: number
 
+
+  /*
+  FILTRO
+  */
+
+
   constructor(
+    private estadoventa : statusVentaservice,
     private ventasproyectoService: VentasproyectoService,
     private activatedRoute: ActivatedRoute,
     public authService: AuthService,
     private router: Router,
     public VentaService: VentaService
+
   ) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((params) => {
       this.paramIdProyecto = parseInt(params.get('id'))
+      this.estadoventaSeleccionado = parseInt(params.get('idestadoventa'))
+
+      this.fechaDesde = (params.get('fechaini'))
+      this.fechaHasta = (params.get('fechafin'))
+
+      this.dpfechaDesde.setValue(this.fechaDesde)
+      this.dpfechaHasta.setValue(this.fechaHasta)
+
     })
     this.obtenerVentas(this.paramIdProyecto)
+
+
+
+
+
   }
 
   ventasLista: Venta[]
@@ -52,6 +76,7 @@ export class VentasProyectoComponent implements OnInit {
         this.id = this.paramIdProyecto
       })
     })
+    this.obtenerEstadoVentas()
   }
 
   status: boolean = false
@@ -62,4 +87,60 @@ export class VentasProyectoComponent implements OnInit {
   selectItemProyecto(idProyecto) {
     this.idProyectoSeleted = idProyecto
   }
+
+  /**
+   * FILTRO ESTADO / FECHA
+   * **/
+
+  estadoventaSeleccionado: number
+  fechaDesde: string
+  fechaHasta: string
+
+  @ViewChild('dpfechaDesde', { static: true }) dpfechaDesde: DatepickerRoundedComponent
+  @ViewChild('dpfechaHasta', { static: true }) dpfechaHasta: DatepickerRoundedComponent
+
+
+  onfechaDesde(newdate: string) {
+    this.fechaDesde = newdate
+    this.obtenerVentasProyecto()
+  }
+
+  onfechaHasta(newdate: string) {
+    this.fechaHasta = newdate
+    this.obtenerVentasProyecto()
+  }
+
+  onChangetEstadoVenta(){
+    this.obtenerVentasProyecto()
+  }
+
+
+  obtenerVentasProyecto(){
+    this.VentaService.getVentasByProyectoEstadoFeciniFecfin(this.paramIdProyecto, this.estadoventaSeleccionado, this.fechaDesde, this.fechaHasta).subscribe((
+      ventasJsonResponse) => {
+        console.info(ventasJsonResponse)
+        this.ventasLista = ventasJsonResponse
+      })
+  }
+
+  public obtenerEstadoVentas(){
+    this.estadoventa.getEstadoVenta().subscribe((response)=> {
+      this.tipoestado = response
+      console.info(this.estadoventa)
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
