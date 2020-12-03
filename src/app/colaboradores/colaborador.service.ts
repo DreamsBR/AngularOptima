39b6ebce952bd05f298995dcd44a9891c15ebeec
raul <raulcarrillo@outlook.com>
2020-12-03 15:17:42 -0500

    agregando seguridad

--- src/app/colaboradores/colaborador.service.ts
+++ src/app/colaboradores/colaborador.service.ts
@@ -11,14 +11,6 @@ import { URL_BACKEND } from '../config/config';
 @Injectable()
 export class ColaboradorService {
 
-
-
-  httpOptions = {
-    headers: new HttpHeaders({
-    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlZXJhem96YW11ZGlvQGdtYWlsLmNvbSIsImlkQ29sYWJvcmFkb3IiOjMsInJvbGVzIjpbIlJPTEVfQURNSU4iXSwiaWF0IjoxNjA3MDE5MTcwLCJleHAiOjE2MDcxMDU1NzB9.-86uGdHEEoXXayFeDOVTZxvDWlZQcS0MxHfkpthYf8ruqdcMcQWFFpFnaKo11efEoH_E1wCEOmrLMqQ-upUo9w'})
-  };
-
-
     private urlEndPoint: string = URL_BACKEND + 'colaborador';
 
     constructor(private http: HttpClient, private router: Router) { }
@@ -63,19 +55,13 @@ export class ColaboradorService {
         );
     }
 
-
-
-
-
     obtenerColaboradorDni(nrdoc):Observable<Colaborador>{
       return this.http.get<Colaborador>(this.urlEndPoint + '/findByNumeroDocumento/' + nrdoc)
     }
 
-
     obtenerColaboradorPorId(idColaborador): Observable<Colaborador> {
         return this.http.get<Colaborador>(this.urlEndPoint + '/' + idColaborador)
     }
-
 
     actualizarColaborador(colaborador: Colaborador, idColaborador: number): Observable<any> {
         return this.http.put<any>(this.urlEndPoint + '/' + idColaborador, colaborador).pipe(
