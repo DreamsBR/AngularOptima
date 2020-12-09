1688216cdaefd94043f1ab52710b7f3345550a09
Alec Gonzalez <alecgz94@gmail.com>
2020-12-09 16:17:13 -0500

    Adjuntar DNI Cliente y Conyuge

--- src/app/clientes-nuevo-editar/clientes-nuevo-editar.component.ts
+++ src/app/clientes-nuevo-editar/clientes-nuevo-editar.component.ts
@@ -10,17 +10,17 @@ import { Tipodocumento } from './tipodocumento'
 import { TipodocumentoService } from './tipodocumento.service'
 import { isNull } from 'util'
 import { DatepickerRoundedComponent } from '../datepicker-rounded/datepicker-rounded.component'
+import { UtilService } from '../util/util.service'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { detectMime } from '../util/detectMime'
 
 @Component({
   selector: 'app-clientes-nuevo-editar',
   templateUrl: './clientes-nuevo-editar.component.html'
 })
-
 export class ClientesNuevoEditarComponent implements OnInit {
-
+  loading: boolean = false
   public cliente: Cliente = new Cliente()
-
-
 
   public clientenodo: Clientenodo = new Clientenodo()
   public errores: string[]
@@ -33,29 +33,36 @@ export class ClientesNuevoEditarComponent implements OnInit {
 
   @ViewChild('dpFechaDeNacimiento', { static: true }) dpFechaDeNacimiento: DatepickerRoundedComponent
 
+  TYPES_UPLOAD_FILE = {
+    KEYCLIENTE: 'CLIENTE',
+    KEYCLIENTE_CONYUGE: 'CLIENTE_CONYUGE'
+  }
+
+  typeUploadSelected: string = null
+
   constructor(
     private clienteService: ClienteService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private estadocivilService: EstadocivilService,
-    private tipodocumentoService: TipodocumentoService
+    private tipodocumentoService: TipodocumentoService,
+    private _snackBar: MatSnackBar,
+    private utilService: UtilService
   ) {}
 
   ngOnInit() {
-
     this.obtenerEstadoCivil()
     this.obtenerTipoDocumento()
 
     this.activatedRoute.paramMap.subscribe((params) => {
       this.idCliente = parseInt(params.get('id'))
-      this.nrodoc = (params.get('nrodoc'))
+      this.nrodoc = params.get('nrodoc')
       this.idproyecto = parseInt(params.get('idproyecto'))
     })
 
-    if( this.idCliente != 0 ){
+    if (this.idCliente != 0) {
       this.clienteService.obtenerClientesPorId(this.idCliente).subscribe(
         (response) => {
-
           this.cliente.apellidos = response.apellidos
           this.cliente.asesor = response.asesor
           this.cliente.conyuge = response.conyuge
@@ -79,16 +86,17 @@ export class ClientesNuevoEditarComponent implements OnInit {
           this.cliente.provincia = response.provincia
           this.cliente.sexo = response.sexo
           this.cliente.telefono = response.telefono
+          this.cliente.fotoDni = response.fotoDni
+          this.cliente.fotoDniConyuge = response.fotoDniConyuge
         },
         (err) => {
           this.errores = err.error.errors as string[]
         }
       )
-    }else{
+    } else {
       this.cliente.idCliente = 0
       this.cliente.idPais = 1
     }
-
   }
 
   public obtenerEstadoCivil() {
@@ -104,18 +112,18 @@ export class ClientesNuevoEditarComponent implements OnInit {
   }
 
   public agregarCliente(): void {
-    if(Object.keys(this.cliente).length < 12){
-      swal('Campos Incompletos de Cliente', '','error')
+    if (Object.keys(this.cliente).length < 14) {
+      swal('Campos Incompletos de Cliente', '', 'error')
       return
     }
 
-    if(this.cliente.idCliente == 0){
+    if (this.cliente.idCliente == 0) {
       this.clienteService.agregarCliente(this.cliente).subscribe(
         (response) => {
-          if(!isNull(this.nrodoc)){
+          if (!isNull(this.nrodoc)) {
             this.router.navigate(['/ventas-proyecto-nuevo-editar/' + this.idproyecto + '/' + response.nroDocumento])
             swal('Nuevo cliente', `Cliente ${response.nombres} creado con exito`, 'success')
-          }else{
+          } else {
             this.router.navigate(['/clientes'])
             swal('Nuevo cliente', `Cliente ${response.nombres} creado con exito`, 'success')
           }
@@ -124,7 +132,7 @@ export class ClientesNuevoEditarComponent implements OnInit {
           this.errores = err.error.errors as string[]
         }
       )
-    }else{
+    } else {
       this.clienteService.actualizarCliente(this.cliente, this.cliente.idCliente).subscribe(
         (response) => {
           this.router.navigate(['/clientes'])
@@ -135,10 +143,9 @@ export class ClientesNuevoEditarComponent implements OnInit {
         }
       )
     }
-
   }
 
-  siguientePagina(tabName: string){
+  siguientePagina(tabName: string) {
     document.getElementById(tabName).click()
   }
 
@@ -152,28 +159,115 @@ export class ClientesNuevoEditarComponent implements OnInit {
     this.status = !this.status
   }
 
-  setFechaNacimiento (date: string) {
-    this.cliente.fechaNacimiento  = date
+  setFechaNacimiento(date: string) {
+    this.cliente.fechaNacimiento = date
   }
 
-
-
-  documentoPersonal(event){
+  documentoPersonal(event) {
     const file = event.target.files[0]
     const reader = new FileReader()
     reader.readAsDataURL(file)
-    reader.onload=()=> {
+    reader.onload = () => {
       console.log(reader.result)
     }
   }
 
-  documentoCony(event){
+  documentoCony(event) {
     const file = event.target.files[0]
     const reader = new FileReader()
     reader.readAsDataURL(file)
-    reader.onload=()=> {
+    reader.onload = () => {
       console.log(reader.result)
     }
   }
 
+  setTypeFileToUpload(type: string) {
+    this.typeUploadSelected = type
+  }
+
+  inputUploadFileChanged(event) {
+    const file = event.target.files[0]
+    if (file) {
+      this.loading = true
+      this.utilService.postUploadFile
+      const extension = file.name.split('.').pop()
+      const reader = new FileReader()
+      const hashRandom = Math.floor(Math.random() * (99999 - 11111)) + 11111
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          this.utilService.postUploadFile(reader.result, `${this.typeUploadSelected}_FILEDOC_RANDOM_${hashRandom}.${extension}`).subscribe(
+            (resp) => {
+              switch (this.typeUploadSelected) {
+                case this.TYPES_UPLOAD_FILE.KEYCLIENTE:
+                  this.cliente.fotoDni = resp.fileName
+                  break
+                case this.TYPES_UPLOAD_FILE.KEYCLIENTE_CONYUGE:
+                  this.cliente.fotoDniConyuge = resp.fileName
+                  break
+              }
+              this.btnCerrarModalSubirArchivo()
+              this.openSnackBar('success', '✓ Archivo subido', 'Cerrar')
+              this.loading = false
+            },
+            (error) => {
+              this.loading = false
+              this.openSnackBar('error', 'No se pudo asociar el archivo a la fecha seleccionada', 'Cerrar')
+              console.log(error)
+            }
+          )
+        } else {
+          this.openSnackBar('error', 'No se pudo convertir a String base 64', 'Cerrar')
+          console.log('No se pudo convertir a String base 64')
+        }
+      }
+    } else {
+      console.log('Archivo vacío. no se sube la ')
+    }
+  }
+
+  btnCerrarModalSubirArchivo() {
+    document.getElementById('btnModalSubirArchivoClose').click()
+  }
+
+  descargarArchivoSubido(fileName: string) {
+    console.log('descargando')
+    this.loading = true
+    this.utilService.postDownloadBase64File(fileName).subscribe(
+      (resp) => {
+        const dataTag = detectMime(resp.fileName)
+        fetch(dataTag + resp.base64)
+          .then((res) => res.blob())
+          .then((blob) => {
+            this.loading = false
+            var link = window.document.createElement('a')
+            link.href = window.URL.createObjectURL(blob)
+            link.target = '_blank'
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+          })
+      },
+      (error) => {
+        this.loading = false
+        console.log(error)
+      }
+    )
+  }
+
+  openSnackBar(type: string, message: string, action: string, duration: number = 5000) {
+    let className = ''
+    switch (type) {
+      case 'success':
+        className = 'agz-snackbar__success'
+        break
+      case 'error':
+        className = 'agz-snackbar__error'
+        break
+    }
+    this._snackBar.open(message, action, {
+      duration: duration,
+      panelClass: [className]
+    })
+  }
 }
