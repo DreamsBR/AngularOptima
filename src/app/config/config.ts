82eaa24ab14a6c1e04a0ab162886f88074b5c29e
raul <raulcarrillo@outlook.com>
2020-12-12 12:54:30 -0500

    agregando variables de entorno de produccion

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,5 @@
-export const URL_BACKEND = 'http://104.248.230.75:8080/services/v1/'
+import { environment } from './../../environments/environment'
+
+export const URL_BACKEND = environment.APIEndpoint + 'services/v1/'
 export const URL_BACKEND_DEMO = '/assets/json/'
-export const URL_BACKEND_SEG = 'http://104.248.230.75:8080/services/'
+export const URL_BACKEND_SEG = environment.APIEndpoint + 'services/'
