c7082ab88a67d75e9eb8a4a2b45d2c540df5b288
raul <raulcarrillo@outlook.com>
2020-12-12 18:38:03 -0500

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // APIEndpoint: 'http://138.197.210.42:8080/'
-  APIEndpoint: 'http://104.248.230.75:8080/'
+  APIEndpoint: 'http://138.197.210.42:8080/'
+  // APIEndpoint: 'http://104.248.230.75:8080/'
 };
